2d1f08c81ef28dfe50663da8175a830ddc739184
NomadesPrime <fernandesrichard312@gmail.com>
2022-10-28 08:23:35 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'safe_map',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
